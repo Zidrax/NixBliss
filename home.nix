d426f36104412038f975d3a42fde231c558370cf
Zidrax <platon.tyurin@gmail.com>
2025-12-28 18:04:45 +0300

    vim settings

--- home.nix
+++ home.nix
@@ -12,4 +12,32 @@
   home.packages = with pkgs; [
     jami
   ];
+
+  # Vim
+  programs.vim = {
+    enable = true;
+
+    #plugins
+    plugins = with pkgs.vimPlugins; [
+      vim-airline      # Красивая статусная строка
+      vim-nix          # Подсветка синтаксиса Nix
+      nerdtree         # Дерево файлов
+    ];
+
+    # Настройки .vimrc в формате Nix
+    settings = {
+      number = true;         # Включить номера строк
+      relativenumber = true; # Относительные номера строк
+      shiftwidth = 2;        # Размер отступа
+      expandtab = true;      # Использовать пробелы вместо табуляции
+      mouse = "a";           # Поддержка мыши
+    };
+    # Дополнительный конфиг (традиционный синтаксис vimrc)
+    extraConfig = ''
+      syntax on
+      set clipboard=unnamedplus
+      " Горячая клавиша для дерева файлов
+      map <C-n> :NERDTreeToggle<CR>
+    '';
+  }
 }
