f7eecacdcc29ee50ecc6672073ca6d04a1f37dfd
Zidrax <platon.tyurin@gmail.com>
2025-12-29 18:10:05 +0300

    firefox settings

--- home.nix
+++ home.nix
@@ -42,37 +42,36 @@
       map <C-n> :NERDTreeToggle<CR>
     '';
   };
-
+  
   programs.firefox = {
     enable = true;
     profiles.User = {
       isDefault = true;
-      
+
       settings = {
-        # --- ИНТЕРФЕЙС И МИНИМАЛИЗМ ---
-        "browser.startup.page" = 0;              # 0 = пустая страница при запуске
-        "browser.startup.homepage" = "about:blank"; 
-        "browser.newtabpage.enabled" = false;     # Чистая новая вкладка (без топов сайтов)
-        
-        "extensions.pocket.enabled" = false;      # Полностью отключить Pocket
-        "browser.tabs.firefox-view" = false;      # Убрать иконку Firefox View (вверху слева)
-        "browser.aboutConfig.showWarning" = false; # Не бесить предупреждением в about:config
-        
-        # --- ОТКЛЮЧЕНИЕ РОЖИ И СПАМА ---
-        "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # Убрать статьи
-        "browser.newtabpage.activity-stream.showSponsored" = false;            # Убрать рекламу
+        # --- ВОССТАНОВЛЕНИЕ СЕССИИ (Твои вкладки) ---
+        "browser.startup.page" = 3;               # 3 = Восстанавливать предыдущую сессию
+        "browser.startup.homepage" = "about:blank"; # Новое окно всё равно будет чистым
+
+        # --- ПАРОЛИ (Раз ты ими пользуешься) ---
+        "signon.rememberSignons" = true;          # Вернул возможность сохранять пароли
+
+        # --- МИНИМАЛИЗМ (Оставляем как было) ---
+        "browser.newtabpage.enabled" = false;
+        "extensions.pocket.enabled" = false;
+        "browser.tabs.firefox-view" = false;
+        "browser.aboutConfig.showWarning" = false;
+
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "browser.vpn_promo.enabled" = false;                                   # Убрать рекламу VPN
+        "browser.vpn_promo.enabled" = false;
         "browser.promo.focus.enabled" = false;
-        
-        # --- ИСТОРИЯ И ПАРОЛИ ---
-        "places.history.enabled" = true;          # ИСТОРИЯ СОХРАНЯЕТСЯ (как ты просил)
-        "signon.rememberSignons" = false;         # Не предлагать сохранять пароли (для минимализма)
-        
-        # --- СИСТЕМНОЕ ---
-        "browser.shell.checkDefaultBrowser" = false; # Не спрашивать про "браузер по умолчанию"
-        "datareporting.healthreport.uploadEnabled" = false; # Меньше телеметрии
+
+        "places.history.enabled" = true;
+        "browser.shell.checkDefaultBrowser" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
       };
     };
-  };  
+  };
 }
