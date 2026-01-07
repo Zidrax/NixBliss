469bb90261ae90033bc18d036d59449d7970e650
Zidrax <platon.tyurin@gmail.com>
2026-01-07 22:27:25 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
+        { nixpkgs.hostPlatform = "x86_64-linux"; }
+
         ./default.nix
         ./hardware-configuration.nix
 
